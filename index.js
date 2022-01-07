1e68b210bd28b6ee344fd6c86c0471c0d1f6fc70
Pirated Gonb <63507206+staticalliam7@users.noreply.github.com>
2022-01-07 18:26:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const { Client, MessageEmbed } = require('discord.js');
+const config = require('./config');
+const commands = require('./help');
+
+let bot = new Client({
+  fetchAllMembers: true, // Remove this if the bot is in large guilds.
+  presence: {
+    status: 'online',
+    activity: {
+      name: `${config.prefix}help`,
+      type: 'LISTENING'
+    }
+  }
+});
+
+bot.on('ready', () => console.log(`Logged in as ${bot.user.tag}.`));
+var gonbcount = 0;
+bot.on('message', async message => {
+  // Check for command
+  if (message.content.startsWith(config.prefix)) {
+    let args = message.content.slice(config.prefix.length).split(' ');
+    let command = args.shift().toLowerCase();
+
+    switch (command) {
+
+
+      case 'gonb':
+        gonbcount++;
+        message.channel.send("Gonb #"+gonbcount);
+        break;
+
+
+    }
+  }
+});
+
+require('./server')();
+bot.login(config.token);
