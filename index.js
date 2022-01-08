8699d30c759195bc6ea9eca483f7ed109c70779d
Pirated Gonb <63507206+staticalliam7@users.noreply.github.com>
2022-01-08 02:37:32 +0000

    Implemented gonb cooldown

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Client, MessageEmbed } = require('discord.js');
 const config = require('./config');
-const commands = require('./help');
 
+const talkedRecently = new Set();
 let bot = new Client({
   fetchAllMembers: true, // Remove this if the bot is in large guilds.
   presence: {
@@ -18,13 +18,25 @@ var gonbcount = 0;
 bot.on('message', async message => {
   // Check for command
   if (message.content.startsWith('gonb')) {
-    
+        if (talkedRecently.has(msg.author.id)) {
+            msg.channel.send(msg.author+"**seems to be very enthusiastic about gonb! I can only count every 5 seconds.**");
+    } else {
+
+           // the user can type the command ... your command code goes here :)
+        gonbcount++;
+        message.channel.send("Gonb #"+gonbcount);
+        
+        talkedRecently.add(msg.author.id);
+        setTimeout(() => {
+   
+          talkedRecently.delete(msg.author.id);
+        }, 5000);
+    }
 
 
 
   
-        gonbcount++;
-        message.channel.send("Gonb #"+gonbcount);
+      
         
 
     
