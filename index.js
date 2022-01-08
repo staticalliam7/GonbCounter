a8dc4debbc7f04eff598887a10a3304d59660856
Pirated Gonb <63507206+staticalliam7@users.noreply.github.com>
2022-01-08 16:25:57 +0000

    changed msg to message cause i'm a moron

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const config = require('./config');
 
 const talkedRecently = new Set();
 let bot = new Client({
-  fetchAllMembers: true, // Remove this if the bot is in large guilds.
+
   presence: {
     status: 'online',
     activity: {
-      name: `${config.prefix}help`,
-      type: 'LISTENING'
+      name: `Counting Gonbs`,
+      type: 'Playing'
     }
   }
 });
@@ -18,18 +18,18 @@ var gonbcount = 0;
 bot.on('message', async message => {
   // Check for command
   if (message.content.startsWith('gonb')) {
-        if (talkedRecently.has(msg.author.id)) {
-            msg.channel.send(msg.author+"**seems to be very enthusiastic about gonb! I can only count every 5 seconds.**");
+        if (talkedRecently.has(message.author.id)) {
+            message.channel.send(message.author+" **seems to be very enthusiastic about gonb! I can only count every 5 seconds.**");
     } else {
 
            // the user can type the command ... your command code goes here :)
         gonbcount++;
         message.channel.send("Gonb #"+gonbcount);
         
-        talkedRecently.add(msg.author.id);
+        talkedRecently.add(message.author.id);
         setTimeout(() => {
    
-          talkedRecently.delete(msg.author.id);
+          talkedRecently.delete(message.author.id);
         }, 5000);
     }
 
